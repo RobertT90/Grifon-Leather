4e4aa8dde499e71700f00678dd2482af5c42de22
Robert <topolovec.r@gmail.com>
2020-03-26 12:28:38 +0100

    added about us responsive

--- js/components/functionality.js
+++ js/components/functionality.js
@@ -41,26 +41,28 @@ var basketCheck = document.querySelector('.basket-check');
 var body = document.querySelector('body');
 const catItem = document.querySelector('.cat-main__inner--item');
 
-
-document.getElementById('basket-button').addEventListener('click', function () {
-    basket.classList.toggle('open');
-    if (document.querySelector('.basket').classList.contains('open')) {
-        body.classList.add('no-scroll');
-    }
-});
-
-document.querySelector('.basket-close').addEventListener('click', function () {
-    basket.classList.remove('open');
-    if (!document.querySelector('.basket').classList.contains('open')) {
-        body.classList.remove('no-scroll');
-    }
-});
-
 document.querySelector('.menu-toggle').addEventListener('click', function () {
     document.querySelector('.menu-toggle').classList.toggle('open');
     document.querySelector('.main-menu').classList.toggle('open');
     body.classList.add('no-scroll');
 });
+
+
+if (document.querySelector('.basket')) {
+    document.getElementById('basket-button').addEventListener('click', function () {
+        basket.classList.toggle('open');
+        if (document.querySelector('.basket').classList.contains('open')) {
+            body.classList.add('no-scroll');
+        }
+    });
+
+    document.querySelector('.basket-close').addEventListener('click', function () {
+        basket.classList.remove('open');
+        if (!document.querySelector('.basket').classList.contains('open')) {
+            body.classList.remove('no-scroll');
+        }
+    });
+}
 
 if (document.querySelector('.check-basket')) {
     document.querySelector('.check-basket').addEventListener('click', function () {
